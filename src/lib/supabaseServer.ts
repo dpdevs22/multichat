29585b04612a36f06a7ef502b9cf46281a0a1a8e
DP Devs <113346393+dpdevs22@users.noreply.github.com>
2025-08-25 14:52:17 +0100

    Updated Supabase Files

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,7 +1,14 @@
+// import { createClient } from '@supabase/supabase-js'
+
+// export function supabaseAdmin() {
+//   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+//   const key = process.env.SUPABASE_SERVICE_ROLE!
+//   return createClient(url, key, { auth: { persistSession: false } })
+// }
+
 import { createClient } from '@supabase/supabase-js'
 
-export function supabaseAdmin() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-  const key = process.env.SUPABASE_SERVICE_ROLE!
-  return createClient(url, key, { auth: { persistSession: false } })
-}
+export const supabaseServer = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+)
